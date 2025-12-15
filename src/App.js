c1dc20f413f138f7db65dd6289617401d8530900
Satish Singh <satishsingh8757@gmail.com>
2025-12-15 07:51:19 +0530

    useQuery implementation

--- src/App.js
+++ src/App.js
@@ -10,27 +10,38 @@ import ErrorPage from "./Pages/ErrorPage";
 import NavBar from "./Pages/NavBar";
 import Profile from "./Pages/StateManagement/Profile";
 import { createContext, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const UserContext = createContext();
 function App() {
+  const client = new QueryClient({
+    defaultOptions: {
+      queries: {
+        refetchOnWindowFocus: false,
+      },
+    },
+  });
+
   const [userName, setUserName] = useState("Satish");
 
   return (
     <UserContext.Provider value={{ userName, setUserName }}>
       <div className="App">
-        <Router>
-          <NavBar />
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/todolist" element={<ToDoList />} />
-            <Route path="/predictage" element={<PredictAgeApiCall />} />
-            <Route path="/catfact" element={<CatFactAPICall />} />
-            <Route path="/person" element={<Person />} />
-            <Route path="/gasplanet" element={<GasPlanet />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="*" element={<ErrorPage />} />
-          </Routes>
-        </Router>
+        <QueryClientProvider client={client}>
+          <Router>
+            <NavBar />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/todolist" element={<ToDoList />} />
+              <Route path="/predictage" element={<PredictAgeApiCall />} />
+              <Route path="/catfact" element={<CatFactAPICall />} />
+              <Route path="/person" element={<Person />} />
+              <Route path="/gasplanet" element={<GasPlanet />} />
+              <Route path="/profile" element={<Profile />} />
+              <Route path="*" element={<ErrorPage />} />
+            </Routes>
+          </Router>
+        </QueryClientProvider>
       </div>
     </UserContext.Provider>
   );
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,12 +1,38 @@
 import React, { useContext } from "react";
 import { UserContext } from "../App";
+import { useQuery } from "@tanstack/react-query";
+import Axios from "axios";
 
 const Home = () => {
+  const catfactUrl = "https://catfact.ninja/fact";
+
+  const fetchData = async (URL) => {
+    const res = await Axios.get(URL);
+    return res.data;
+  };
+  const {
+    data,
+    error,
+    isLoading,
+    refetch: getCatData,
+  } = useQuery({
+    queryKey: ["cat"],
+    queryFn: () => {
+      return fetchData(catfactUrl);
+    },
+  });
+
   const { userName } = useContext(UserContext);
   return (
     <div>
       Home Page
       <div>User : {userName}</div>
+      <div>Cat Fact : {data?.fact}</div>
+      {isLoading && <p>Data Loading</p>}
+      {error && error?.message}
+      <div>
+        <button onClick={getCatData}>Get New Cat Fact</button>
+      </div>
     </div>
   );
 };
